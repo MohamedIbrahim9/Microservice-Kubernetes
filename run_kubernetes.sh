c3518d4a984704fea59e0cec33c8b19d8d42b350
mohamedIbrahim <engmemo158@gmail.com>
2020-09-22 15:18:16 +0200

    add kubernetes and docker scripts

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,18 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=mohamedibrahim9/microservice-kubernetes
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run my-python-app --image=$dockerpath --port=8000
 
+# 20 Sec sleep to wait for pods 
+echo "Please wait for pods to be available"
+sleep 20s
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward my-python-app 8000:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=mohamedibrahim9/microservice-kubernetes
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login 
+docker tag my-python-app $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
